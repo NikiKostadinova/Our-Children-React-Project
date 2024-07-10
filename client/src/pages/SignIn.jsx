c159b5c6db4068a63e09d37c127a912d1f9bdf24
NikiKostadinova <niki_radost@yahoo.com>
2024-07-10 10:20:22 +0300

    SignIn

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 
-export default function SignUp() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -15,13 +15,13 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.username || !formData.email || !formData.password) {
+    if (!formData.email || !formData.password) {
       return setErrorMessage('Please fill out all fields!');
     }
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
@@ -32,7 +32,7 @@ export default function SignUp() {
       }
       setLoading(false);
       if(res.ok){
-        navigate("/sign-in");
+        navigate("/");
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -56,16 +56,7 @@ export default function SignUp() {
         </div>
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <div>
-              <Label value="Your username" />
-              <TextInput
-                type="text"
-                placeholder="Username"
-                id="username"
-                onChange={handleChange}
-              />
-            </div>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>           
             <div>
               <Label value="Your email" />
               <TextInput
@@ -79,7 +70,7 @@ export default function SignUp() {
               <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="Password"
+                placeholder="**********"
                 id="password"
                 onChange={handleChange}
               />
@@ -91,13 +82,13 @@ export default function SignUp() {
                     <Spinner size="sm" />
                     <span className="pl-3">Loading...</span>
                   </>
-                ) : "Sign Up"
+                ) : "Sign In"
               }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-2">
-            <span>Have an account?</span>
-            <Link to="/sign-in" className="text-blue-500">Sign In</Link>          </div>
+            <span>Dont have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">Sign Up</Link>          </div>
         </div>
         <div>
           {
